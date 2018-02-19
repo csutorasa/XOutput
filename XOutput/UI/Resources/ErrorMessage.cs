02a3e05ea749249e2f1c45787dcf546079f929a6
Ármin <csutorasa@gmail.com>
2018-02-19 21:22:58 +0100

    VIGEm error message fix

--- XOutput/UI/Resources/ErrorMessage.cs
+++ XOutput/UI/Resources/ErrorMessage.cs
@@ -12,6 +12,6 @@ namespace XOutput.UI.Resources
         public static string Error { get { return "Error"; } }
         public static string SaveSettingsError { get { return "Failed to save {0}!" + Environment.NewLine + "{1}"; } }
         public static string LoadSettingsError { get { return "Failed to load {0}!" + Environment.NewLine + "{1}"; } }
-        public static string SCPNotInstalledError { get { return "Please download and install ScpToolkit!" + Environment.NewLine + "{0}"; } }
+        public static string SCPNotInstalledError { get { return "Please install VIGEm!" + Environment.NewLine + "{0}"; } }
     }
 }
