4fcb7ca37d7abe8d2a9f8724f9ba0fa3a8b80c40
Ármin <csutorasa@gmail.com>
2018-03-29 23:38:30 +0200

    Version update

--- XOutput/UpdateChecker/Version.cs
+++ XOutput/UpdateChecker/Version.cs
@@ -15,7 +15,7 @@ namespace XOutput.UpdateChecker
         /// <summary>
         /// Current application version.
         /// </summary>
-        public const string AppVersion = "3.5";
+        public const string AppVersion = "3.6";
 
         private static readonly ILogger logger = LoggerFactory.GetLogger(typeof(Version));
 
