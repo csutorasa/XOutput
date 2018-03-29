d12c58e387fee65c66d8fca4bf8cdfe1557f8f0a
Ármin <csutorasa@gmail.com>
2018-03-29 21:40:10 +0200

    Added basic application logging feature

---
+++ XOutput/Logging/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOutput.Logging
+{
+    public interface ILogger
+    {
+        void Info(string log, params string[] args);
+        void Warning(string log, params string[] args);
+        void Error(string log, params string[] args);
+        void Error(Exception ex);
+    }
+}
---
+++ XOutput/Logging/LoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOutput.Logging
+{
+    public static class LoggerFactory
+    {
+        public static ILogger GetLogger(Type type)
+        {
+            return new TraceLogger(type);
+        }
+    }
+}
