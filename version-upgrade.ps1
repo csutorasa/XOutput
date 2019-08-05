1e8fbf435923ab6a1c7d90d7901dc29684571459
csutorasa <csutorasa@gmail.com>
2019-08-05 20:55:11 +0200

    Prepare for Chocolatey

---
+++ version-upgrade.ps1
@@ -0,0 +1,19 @@
+<#
+    .EXAMPLE
+      .\version-upgrade.ps1
+      .\version-upgrade.ps1 -version 3.23
+      Set-ExecutionPolicy Bypass -Scope Process -Force; .\version-upgrade.ps1
+#>
+
+param (
+    [Parameter(Mandatory=$true)][string]$version
+)
+
+$new_content = Get-Content .\XOutput\UpdateChecker\Version.cs | ForEach-Object {$_ -replace "public const string AppVersion = `".*`";","public const string AppVersion = `"$version`";"}
+$new_content | Out-File -Encoding utf8 -FilePath .\XOutput\UpdateChecker\Version.cs
+
+$new_content = Get-Content .\appveyor.yml | ForEach-Object {$_ -replace "version: .*-{branch}-{build}","version: $version-{branch}-{build}"}
+[IO.File]::WriteAllLines("$pwd\appveyor.yml", $new_content)
+
+$new_content = Get-Content .\xoutput.nuspec | ForEach-Object {$_ -replace "<version>.*</version>","<version>$version</version>"}
+[IO.File]::WriteAllLines("$pwd\xoutput.nuspec", $new_content)
