d8e29aef99216047a6ec07290b50560e018ad86e
csutorasa <csutorasa@gmail.com>
2019-08-08 15:27:49 +0200

    Fix versioning

--- version-upgrade.ps1
+++ version-upgrade.ps1
@@ -13,6 +13,7 @@ $new_content = Get-Content .\XOutput\UpdateChecker\Version.cs | ForEach-Object {
 $new_content | Out-File -Encoding utf8 -FilePath .\XOutput\UpdateChecker\Version.cs
 
 $new_content = Get-Content .\appveyor.yml | ForEach-Object {$_ -replace "version: .*-{branch}-{build}","version: $version-{branch}-{build}"}
+$new_content = $new_content | ForEach-Object {$_ -replace "versionnumber: .*","versionnumber: $version"}
 [IO.File]::WriteAllLines("$pwd\appveyor.yml", $new_content)
 
 $new_content = Get-Content .\xoutput.nuspec | ForEach-Object {$_ -replace "<version>.*</version>","<version>$version</version>"}
