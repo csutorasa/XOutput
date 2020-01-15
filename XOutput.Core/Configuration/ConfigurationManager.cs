6ef113ef9f31786a199b4fa6e495df1a993b2d0e
csutorasa <csutorasa@gmail.com>
2020-01-15 23:17:56 +0100

    File watcher added

--- XOutput.Core/Configuration/ConfigurationManager.cs
+++ XOutput.Core/Configuration/ConfigurationManager.cs
@@ -24,6 +24,27 @@ namespace XOutput.Core.Configuration
             return default;
         }
 
+        public IDisposable Watch(string filePath, Action<string> handler)
+        {
+            return Watch(Path.GetDirectoryName(filePath), Path.GetFileName(filePath), handler);
+        }
+
+        public IDisposable Watch(string directory, string filter, Action<string> handler) {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = directory;
+            watcher.Filter = filter;
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Changed += (sender, args) => handler.Invoke(args.FullPath);
+            watcher.Created += (sender, args) => handler.Invoke(args.FullPath);
+            watcher.Deleted += (sender, args) => handler.Invoke(args.FullPath);
+            watcher.Renamed += (sender, args) => { handler.Invoke(args.OldFullPath); handler.Invoke(args.OldFullPath); };
+
+            watcher.EnableRaisingEvents = true;
+            return watcher;
+        }
+
         protected abstract string ConfigurationToString<T>(T configuration) where T : IConfiguration;
         protected abstract T StringToConfiguration<T>(string configuration) where T : IConfiguration;
     }
