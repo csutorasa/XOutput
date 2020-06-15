339afc64dff2bea3e668729fcc9c0b4e56e50b8e
csutorasa <csutorasa@gmail.com>
2020-06-15 23:12:49 +0200

    Internet IP address selection

--- XOutput.App/MainWindow.xaml.cs
+++ XOutput.App/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using XOutput.Core.DependencyInjection;
@@ -25,17 +28,30 @@ namespace XOutput.App
 #endif
         }
 
-        private void OpenClick(object sender, RoutedEventArgs e)
+        private async Task OpenClick(object sender, RoutedEventArgs e)
         {
-            var ipAddresses = NetworkInterface.GetAllNetworkInterfaces()
-               .Where(i => i.NetworkInterfaceType == NetworkInterfaceType.Ethernet || i.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
-               .Where(i => i.OperationalStatus == OperationalStatus.Up)
-               .SelectMany(i => i.GetIPProperties().UnicastAddresses)
-               .Select(c => c.Address)
-               .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
-               .Select(a => a.ToString())
-               .ToList();
-            var address = ipAddresses.FirstOrDefault() ?? "localhost";
+            string address;
+            try
+            {
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                {
+                    await socket.ConnectAsync("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    address = endPoint.Address.ToString();
+                }
+            } 
+            catch
+            {
+                var ipAddresses = NetworkInterface.GetAllNetworkInterfaces()
+                   .Where(i => i.NetworkInterfaceType == NetworkInterfaceType.Ethernet || i.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                   .Where(i => i.OperationalStatus == OperationalStatus.Up)
+                   .SelectMany(i => i.GetIPProperties().UnicastAddresses)
+                   .Select(c => c.Address)
+                   .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
+                   .Select(a => a.ToString())
+                   .ToList();
+                address = ipAddresses.FirstOrDefault() ?? "localhost";
+            }
             int port = 8000;
             var process = commandRunner.CreatePowershell($"Start \"http://{address}:{port}\"");
             commandRunner.RunProcess(process);
