25a38ccd5afc2a856e846078bdb0d22b41d7758d
csutorasa <csutorasa@gmail.com>
2021-01-01 18:40:00 +0100

    Update CI

--- version-upgrade.ps1
+++ version-upgrade.ps1
@@ -1,7 +1,7 @@
 <#
     .EXAMPLE
       .\version-upgrade.ps1
-      .\version-upgrade.ps1 -version 3.23
+      .\version-upgrade.ps1 -version 4.0.0
       Set-ExecutionPolicy Bypass -Scope Process -Force; .\version-upgrade.ps1
 #>
 
@@ -9,9 +9,11 @@ param (
     [Parameter(Mandatory=$true)][string]$version
 )
 $ErrorActionPreference = 'Stop';
-$new_content = Get-Content .\XOutput\UpdateChecker\Version.cs | ForEach-Object {$_ -replace "public const string AppVersion = `".*`";","public const string AppVersion = `"$version`";"}
-$new_content | Out-File -Encoding utf8 -FilePath .\XOutput\UpdateChecker\Version.cs
+Get-Content .\.github\workflows\build.yml | `
+ForEach-Object {$_ -replace "  VERSION: .*","  VERSION: $version"} | `
+Out-File -Encoding utf8 -FilePath .\.github\workflows\build.yml
 
-$new_content = Get-Content .\appveyor.yml | ForEach-Object {$_ -replace "version: .*-{branch}-{build}","version: $version-{branch}-{build}"}
-$new_content = $new_content | ForEach-Object {$_ -replace "versionnumber: .*","versionnumber: $version"}
-[IO.File]::WriteAllLines("$pwd\appveyor.yml", $new_content)
+[IO.File]::WriteAllLines("$pwd\appveyor.yml", ` 
+Get-Content .\appveyor.yml | `
+ForEach-Object {$_ -replace "version: .*-{branch}-{build}","version: $version-{branch}-{build}"} | `
+ForEach-Object {$_ -replace "versionnumber: .*","versionnumber: $version"} )
