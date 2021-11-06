3153d25581d08808a0dff562f9e9b3be38ba134f
csutorasa <csutorasa@gmail.com>
2021-11-06 18:46:31 +0100

    chore: fix rename

--- @xoutput/client/src/rest/emulation/emulationClient.ts
+++
@@ -1,8 +0,0 @@
-import { EmulatorResponse } from '@xoutput/api';
-import { http } from '../http';
-
-export const emulationClient = {
-  getInfo(): Promise<EmulatorResponse> {
-    return http.get<EmulatorResponse>('/emulators');
-  },
-};
