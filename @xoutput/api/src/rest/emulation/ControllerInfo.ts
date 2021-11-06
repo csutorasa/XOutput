28976ef274c22890158e9f5cd9ff471f521385ff
csutorasa <csutorasa@gmail.com>
2021-11-06 19:10:50 +0100

    fix: controller api name

--- @xoutput/api/src/rest/emulation/ControllerInfo.ts
+++ @xoutput/api/src/rest/emulation/ControllerInfo.ts
@@ -4,6 +4,7 @@ import { DeviceTypes } from '../../common/DeviceTypes';
 export type ControllerInfo = {
   id: string;
   address?: string;
+  name: string;
   deviceType: DeviceTypes;
   emulator: Emulators;
   active: boolean;
