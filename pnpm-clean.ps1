9d511943b1c65babfd29f4516eb690f35a84887f
Ármin <csutorasa@gmail.com>
2023-11-07 00:22:48 +0100

    Fix build order

---
+++ pnpm-clean.ps1
@@ -0,0 +1,5 @@
+Remove-Item .\@xoutput\api\node_modules\ -Recurse
+Remove-Item .\@xoutput\api\lib\ -Recurse
+Remove-Item .\@xoutput\client\node_modules\ -Recurse
+Remove-Item .\@xoutput\client\lib\ -Recurse
+Remove-Item .\@xoutput\webapp\node_modules\ -Recurse
