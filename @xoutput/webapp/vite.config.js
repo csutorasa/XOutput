804ca1181f2804100b5485c8cfaf00cf7d073a0c
Ármin <csutorasa@gmail.com>
2023-11-07 10:57:00 +0100

    Update webapp to vite

---
+++ @xoutput/webapp/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve, join } from 'path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: join(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'webapp'),
+    target: 'es2021',
+  },
+  plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': 'http://localhost:8000',
+      '/websocket': {
+        target: 'ws://localhost:8000',
+        ws: true,
+      },
+    },
+  }
+});
